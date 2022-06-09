8051c37f29304149e286890fdedf764f89322dc1
gusrnr98 <gusrnr9878@gmail.com>
2022-06-09 15:33:19 +0900

    Revert "project"
    This reverts commit 103fb5ff2a08242166826384829bef76a7433113.

--- NewsTest_V2/app/src/main/java/com/techtown/newstest_v2/NetResult.java
+++
@@ -1,6 +0,0 @@
-package com.techtown.newstest_v2;
-
-public class NetResult {
-    String code = "";
-    Object result;
-}
--- NewsTest_V2/app/src/main/java/com/techtown/newstest_v2/NewsList.java
+++
@@ -1,7 +0,0 @@
-package com.techtown.newstest_v2;
-
-import java.util.ArrayList;
-
-public class NewsList {
-    public ArrayList<Item> news = new ArrayList<>();
-}
